71be0f7ea5112b3d0d6a365da109b560c61622f7
Michael Cheng <mcheng.work@gmail.com>
2025-12-01 15:32:16 +0800

    Initial Commit

---
+++ day1/__test__/day1.spec.ts
@@ -0,0 +1,8 @@
+import { describe, expect, test } from 'vitest'
+import { solution } from '../day1'
+
+describe('day1 tests', () => {
+    test('day1', () => {
+        expect(solution([])).toBe(0);
+    })
+})
---
+++ day1/day1.ts
@@ -0,0 +1,5 @@
+export const solution = (steps:string[]) => {
+    console.log("Day 1 solution");
+
+    return 0;
+};
