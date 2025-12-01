fc38bc09df92d8569e09dfb482f59d3c346f557f
Michael Cheng <mcheng.work@gmail.com>
2025-12-01 20:13:25 +0800

    Solve for Part 2 of Day 1

--- day1/__test__/day1.spec.ts
+++ day1/__test__/day1.spec.ts
@@ -3,32 +3,34 @@ import { solution } from '../day1'
 
 describe('Day1 Tests', () => {
     test('No actions', () => {
-        expect(solution([])).toEqual({ dialPosition: 50, password: 0 });
+        expect(solution([])).include({ dialPosition: 50 });
     })
 
     test('Moving right increments the dial number', () => {
-        expect(solution(['R10'])).toEqual({ dialPosition: 60, password: 0 });
+        expect(solution(['R10'])).include({ dialPosition: 60 });
     })
 
     test('Moving left decrements the dial number', () => {
-        expect(solution(['L20'])).toEqual({ dialPosition: 30, password: 0 });
+        expect(solution(['L20'])).include({ dialPosition: 30 });
     })
 
     test('Moving right past 99', () => {
-        expect(solution(['R50'])).toEqual({ dialPosition: 0, password: 1 });
-        expect(solution(['R60'])).toEqual({ dialPosition: 10, password: 0 });
-        expect(solution(['R210'])).toEqual({ dialPosition: 60, password: 0 });
+        expect(solution(['R50'])).include({ dialPosition: 0 });
+        expect(solution(['R60'])).include({ dialPosition: 10 });
+        expect(solution(['R210'])).include({ dialPosition: 60 });
     })
 
     test('Moving left past 0', () => {
-        expect(solution(['L50'])).toEqual({ dialPosition: 0, password: 1 });
-        expect(solution(['L60'])).toEqual({ dialPosition: 90, password: 0 });
-        expect(solution(['L68'])).toEqual({ dialPosition: 82, password: 0 });
-        expect(solution(['L220'])).toEqual({ dialPosition: 30, password: 0 });
+        expect(solution(['L50'])).include({ dialPosition: 0, password: 1 });
+        expect(solution(['L60'])).include({ dialPosition: 90 });
+        expect(solution(['L68'])).include({ dialPosition: 82 });
+        expect(solution(['L220'])).include({ dialPosition: 30 });
     })
 
     test('Multiple movements', () => {
-        expect(solution(['L68', 'L30', 'R48', 'L5'])).toEqual({ dialPosition: 95, password: 1 });
-        expect(solution(['L68', 'L30', 'R48', 'L5', 'R60', 'L55', 'L1', 'L99', 'R14', 'L82'])).toEqual({ dialPosition: 32, password: 3 });
+        expect(solution(['L68', 'L30'])).include({ dialPosition: 52, password: 0, '0x434C49434B': 1 });
+        expect(solution(['L68', 'L30', 'R48'])).include({ dialPosition: 0, password: 1, '0x434C49434B': 2 });
+        expect(solution(['L68', 'L30', 'R48', 'L5', 'R60', 'L55', 'L1', 'L99', 'R14', 'L82'])).include({ dialPosition: 32, password: 3, '0x434C49434B': 6 });
+        expect(solution(['R1000'])).include({ dialPosition: 50, password: 0, '0x434C49434B': 10 });
     })
 })
--- day1/day1.ts
+++ day1/day1.ts
@@ -1,12 +1,16 @@
 export const solution = (steps:string[]) => {
     let startingPosition = 50
-    const result = { dialPosition: startingPosition, password: 0 }
+    const result = { dialPosition: startingPosition, password: 0, '0x434C49434B': 0 }
 
     for (const step of steps) {
         const direction = step.charAt(0)
         let value = parseInt(step.slice(1), 10)
+        const startingPos = result.dialPosition
 
         if (value > 100) {
+            const fullRotations = Math.floor(value / 100)
+            // console.log('Full rotations:', fullRotations)
+            result['0x434C49434B'] += fullRotations
             value = value % 100
         }
 
@@ -18,12 +22,22 @@ export const solution = (steps:string[]) => {
 
         if (result.dialPosition < 0) {
             result.dialPosition += 100
+            if (result.dialPosition !== 0 && startingPos !== 0) {
+                // console.log(step, 'Underflow adjust + 1', result.dialPosition)
+                result['0x434C49434B'] += 1
+            }
         } else if (result.dialPosition > 99) {
             result.dialPosition -= 100
+            if (result.dialPosition !== 0 && startingPos !== 0) {
+                // console.log(step, 'Overflow adjust + 1', result.dialPosition)
+                result['0x434C49434B'] += 1
+            }
         }
 
         if (result.dialPosition === 0) {
             result.password += 1
+            // console.log(step, 'Lands on Zero + 1', result.dialPosition)
+            result['0x434C49434B'] += 1
         }
     }
 
