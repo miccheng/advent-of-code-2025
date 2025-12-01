6acaa8cbee9c4f2ab17cb537b11edf92a21607f1
Michael Cheng <mcheng.work@gmail.com>
2025-12-01 16:03:51 +0800

    Day 1 part 1

--- day1/__test__/day1.spec.ts
+++ day1/__test__/day1.spec.ts
@@ -1,8 +1,34 @@
 import { describe, expect, test } from 'vitest'
 import { solution } from '../day1'
 
-describe('day1 tests', () => {
-    test('day1', () => {
-        expect(solution([])).toBe(0);
+describe('Day1 Tests', () => {
+    test('No actions', () => {
+        expect(solution([])).toEqual({ dialPosition: 50, password: 0 });
+    })
+
+    test('Moving right increments the dial number', () => {
+        expect(solution(['R10'])).toEqual({ dialPosition: 60, password: 0 });
+    })
+
+    test('Moving left decrements the dial number', () => {
+        expect(solution(['L20'])).toEqual({ dialPosition: 30, password: 0 });
+    })
+
+    test('Moving right past 99', () => {
+        expect(solution(['R50'])).toEqual({ dialPosition: 0, password: 1 });
+        expect(solution(['R60'])).toEqual({ dialPosition: 10, password: 0 });
+        expect(solution(['R210'])).toEqual({ dialPosition: 60, password: 0 });
+    })
+
+    test('Moving left past 0', () => {
+        expect(solution(['L50'])).toEqual({ dialPosition: 0, password: 1 });
+        expect(solution(['L60'])).toEqual({ dialPosition: 90, password: 0 });
+        expect(solution(['L68'])).toEqual({ dialPosition: 82, password: 0 });
+        expect(solution(['L220'])).toEqual({ dialPosition: 30, password: 0 });
+    })
+
+    test('Multiple movements', () => {
+        expect(solution(['L68', 'L30', 'R48', 'L5'])).toEqual({ dialPosition: 95, password: 1 });
+        expect(solution(['L68', 'L30', 'R48', 'L5', 'R60', 'L55', 'L1', 'L99', 'R14', 'L82'])).toEqual({ dialPosition: 32, password: 3 });
     })
 })
--- day1/day1.ts
+++ day1/day1.ts
@@ -1,5 +1,31 @@
 export const solution = (steps:string[]) => {
-    console.log("Day 1 solution");
+    let startingPosition = 50
+    const result = { dialPosition: startingPosition, password: 0 }
 
-    return 0;
+    for (const step of steps) {
+        const direction = step.charAt(0)
+        let value = parseInt(step.slice(1), 10)
+
+        if (value > 100) {
+            value = value % 100
+        }
+
+        if (direction === 'R') {
+            result.dialPosition += value
+        } else if (direction === 'L') {
+            result.dialPosition -= value
+        }
+
+        if (result.dialPosition < 0) {
+            result.dialPosition += 100
+        } else if (result.dialPosition > 99) {
+            result.dialPosition -= 100
+        }
+
+        if (result.dialPosition === 0) {
+            result.password += 1
+        }
+    }
+
+    return result;
 };
