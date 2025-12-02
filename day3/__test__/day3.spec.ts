98a45c90fa1ae2ec003f865f4b0f4383dbe6f7bf
Michael Cheng <mcheng.work@gmail.com>
2025-12-02 23:08:49 +0800

    Day 3 placeholder

---
+++ day3/__test__/day3.spec.ts
@@ -0,0 +1,8 @@
+import { describe, expect, test } from 'vitest'
+import { solutionPart1 } from '../day3'
+
+describe('Day3 Tests', () => {
+    test('No actions', () => {
+        expect(solutionPart1('')).toEqual({});
+    })
+})
---
+++ day3/day3.ts
@@ -0,0 +1,3 @@
+export const solutionPart1 = (input: string) => {
+    return {}
+}
