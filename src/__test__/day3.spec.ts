ba31700f9ebc473d8ed89e4a453147ad60c4e1e1
Michael Cheng <mcheng.work@gmail.com>
2025-12-04 00:33:03 +0800

    Day 3 Part 2 solution

--- src/__test__/day3.spec.ts
+++ src/__test__/day3.spec.ts
@@ -1,22 +1,49 @@
 import { describe, expect, test } from 'vitest'
-import { solutionPart1, largestPossibleJolatage } from '../day3'
+import { solutionPart1, largestPossibleJoltage, solutionPart2, largest12CellJoltage, highestCellCount } from '../day3'
 
 describe('Day3 Tests', () => {
     test('Example', () => {
         expect(solutionPart1("987654321111111\n811111111111119\n234234234234278\n818181911112111")).toEqual(357);
+        expect(solutionPart2("987654321111111\n811111111111119\n234234234234278\n818181911112111")).toEqual(3121910778619);
     })
 })
 
-describe('largestPossibleJolatage', () => {
+describe('largestPossibleJoltage', () => {
     test('calculates highest joltage', () => {
-        expect(largestPossibleJolatage('987654321111111')).toEqual(98)
-        expect(largestPossibleJolatage('811111111111119')).toEqual(89)
-        expect(largestPossibleJolatage('234234234234278')).toEqual(78)
-        expect(largestPossibleJolatage('818181911112111')).toEqual(92)
-        expect(largestPossibleJolatage('891111111111111')).toEqual(91)
+        expect(largestPossibleJoltage('987654321111111')).toEqual(98)
+        expect(largestPossibleJoltage('811111111111119')).toEqual(89)
+        expect(largestPossibleJoltage('234234234234278')).toEqual(78)
+        expect(largestPossibleJoltage('818181911112111')).toEqual(92)
+        expect(largestPossibleJoltage('891111111111111')).toEqual(91)
     })
 
     test('Test input', () => {
-        expect(largestPossibleJolatage('2331221221361221232332583266422231222315311212133222227552392222213223325332632323212227323432113121')).toEqual(97)
+        expect(largestPossibleJoltage('2331221221361221232332583266422231222315311212133222227552392222213223325332632323212227323432113121')).toEqual(97)
+    })
+})
+
+describe('largest12CellJoltage', () => {
+    test('exactly 12 characters', () => {
+        expect(largest12CellJoltage('123456789012')).toEqual(123456789012)
+    })
+
+    test('calculates largest 12 cell joltage', () => {
+        expect(largest12CellJoltage('987654321111111')).toEqual(987654321111)
+        expect(largest12CellJoltage('811111111111119')).toEqual(811111111119)
+        expect(largest12CellJoltage('234234234234278')).toEqual(434234234278)
+        expect(largest12CellJoltage('818181911112111')).toEqual(888911112111)
+    })
+
+    test('highestCellCount', () => {
+        const input = '2222213223325332632323212227323432113121'
+        const result = highestCellCount(input)
+        expect(result).toEqual(27)
+        expect(input[result]).toEqual('7')
+        expect(input.substring(result)).toEqual('7323432113121')
+
+    })
+
+    test('Test input', () => {
+        expect(largest12CellJoltage('2331221221361221232332583266422231222315311212133222227552392222213223325332632323212227323432113121')).toEqual(973432113121)
     })
 })
--- src/day3.ts
+++ src/day3.ts
@@ -1,10 +1,16 @@
 export const solutionPart1 = (input: string): number => {
     return input.trim().split("\n").reduce((acc, joltageRating) => {
-        return acc + largestPossibleJolatage(joltageRating)
+        return acc + largestPossibleJoltage(joltageRating)
     }, 0)
 }
 
-export const largestPossibleJolatage = (input: string): number => {
+export const solutionPart2 = (input: string): number => {
+    return input.trim().split("\n").reduce((acc, joltageRating) => {
+        return acc + largest12CellJoltage(joltageRating)
+    }, 0)
+}
+
+export const largestPossibleJoltage = (input: string): number => {
     const candidates = input.split('')
     const result: { left: number, right: number } = {left: -1, right: -1}
 
@@ -36,4 +42,88 @@ export const largestPossibleJolatage = (input: string): number => {
     // console.log(result)
 
     return parseInt(`${result.left}${result.right}`)
+}
+
+export const largest12CellJoltage = (input: string): number => {
+    // Exactly 12 chars
+    if (input.length === 12) return parseInt(input)
+
+    let lhs = ''
+    let candidateString = input
+    console.log('Starting', candidateString)
+    for(let i = 0; i < 12; i++) {
+        let nextHighestBattery = highestCellCount(candidateString, (12-lhs.length))
+        console.log(nextHighestBattery)
+
+        console.log('Next', `${lhs}${candidateString.substring(nextHighestBattery)}`)
+
+        lhs = `${lhs}${candidateString[nextHighestBattery]}`
+        candidateString = candidateString.substring(nextHighestBattery+1)
+        console.log('LHS', lhs, 'Str', candidateString)
+    }
+
+    return parseInt(lhs)
+
+
+    // // Find indexOf highest number
+    // const highestCellPos = highestCellCount(input)
+    // // Set 1st char as highest number
+    // // let lhs = input[highestCellPos]
+
+    // // let candidateString = input.slice(highestCellPos)
+    // console.log('Candidate String', candidateString)
+    // // while(true) {
+    //     console.log('Foo', candidateString.substring(1))
+    //     let pointer = highestCellCount(candidateString.substring(1))
+    //     console.log(pointer)
+    //     console.log('Candidate String', candidateString)
+    //     // if (pointer < 0) break
+    //     console.log(candidateString.substring(1)[pointer])
+
+    //     lhs = `${lhs}${candidateString[pointer]}`
+    //     candidateString = candidateString.substring(pointer)
+
+    //     console.log('Candidate String', candidateString)
+    //     console.log('LHS', lhs)        
+    // // }
+
+    // // console.log('Candidate String', candidateString)
+    // // console.log('LHS', lhs)
+
+    // let highestNumber = parseInt(candidateString.substring(0, 12))
+    // console.log('Highest Number', highestNumber)
+    // // for(let i=1; i < candidateString.length; i++) {
+    // //     const num = `${lhs}${candidateString.slice(i)}`.substring(0, 12)
+    // //     if (num.length < 12) break
+    // //     console.log('LHS', `${lhs}_${candidateString.slice(i)}`.substring(0, 13), 'Checking:', num, 'against', highestNumber)
+
+    // //     if (parseInt(num) > highestNumber) {
+    // //         highestNumber = parseInt(num)
+    // //         console.log('Adding to LHS:', candidateString[i])
+    // //         lhs = `${lhs}${candidateString[i]}`
+    // //     }
+    // // }
+    // // 92222213223325332632323212227323432113121
+    // // 97432113121 x less than 12
+    // // 97323432113121 (14)
+    // // 973_234321131 (12)
+    // // 973_343211312 (12)
+    // // 973_432113121 (12)
+    // // 973432113121
+    // // 973432113121
+
+    // return highestNumber
+    return 0
+}
+
+export const highestCellCount = (input: string, minimumLength=12): number => {
+    const nums = ['9', '8', '7', '6', '5', '4', '3', '2', '1']
+    for (const n of nums) {
+        const pos = input.indexOf(n)
+        if (pos > -1 && (input.length - pos) >= minimumLength) {
+            return input.indexOf(n)
+        }
+    }
+
+    return -1
 }
