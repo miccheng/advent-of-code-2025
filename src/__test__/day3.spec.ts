50d4b238127c356615b6586698fcf5a12b793f97
Michael Cheng <mcheng.work@gmail.com>
2025-12-03 14:00:02 +0800

    Day 3: Attempt 1

--- src/__test__/day3.spec.ts
+++ src/__test__/day3.spec.ts
@@ -1,8 +1,17 @@
 import { describe, expect, test } from 'vitest'
-import { solutionPart1 } from '../day3'
+import { solutionPart1, largestPossibleJolatage } from '../day3'
 
 describe('Day3 Tests', () => {
-    test('No actions', () => {
-        expect(solutionPart1('')).toEqual({});
+    test('Example', () => {
+        expect(solutionPart1("987654321111111\n811111111111119\n234234234234278\n818181911112111")).toEqual(357);
+    })
+})
+
+describe('largestPossibleJolatage', () => {
+    test('calculates highest joltage', () => {
+        expect(largestPossibleJolatage('987654321111111')).toEqual(98)
+        expect(largestPossibleJolatage('811111111111119')).toEqual(89)
+        expect(largestPossibleJolatage('234234234234278')).toEqual(78)
+        expect(largestPossibleJolatage('818181911112111')).toEqual(92)
     })
 })
--- src/day3.ts
+++ src/day3.ts
@@ -1,3 +1,38 @@
-export const solutionPart1 = (input: string) => {
-    return {}
+export const solutionPart1 = (input: string): number => {
+    return input.trim().split("\n").reduce((acc, joltageRating) => {
+        return acc + largestPossibleJolatage(joltageRating)
+    }, 0)
+}
+
+export const largestPossibleJolatage = (input: string): number => {
+    const candidates = input.split('')
+    const result: { left: number, right: number } = {left: -1, right: -1}
+
+    const firstTwo = candidates.slice(0, 2)
+    result.left = parseInt(firstTwo[0])
+    result.right = parseInt(firstTwo[1])
+
+    for(let i = 2; i < candidates.length; i++) {
+        // console.log('checking', candidates[i], 'against', result)
+
+        const checkValue = parseInt(candidates[i])
+        
+        if (checkValue > result.left) {
+            if (i !== (candidates.length-1)) {
+                // New higher value, make this the new LHS and next char is RHS
+                result.left = checkValue
+                result.right = parseInt(candidates[i+1])
+                i++
+            } else {
+                // If this is the last char, make it the RHS value
+                result.right = checkValue
+            }
+        } else if (checkValue > result.right) {
+            result.right = checkValue
+        }
+    }
+
+    // console.log(result)
+
+    return parseInt(`${result.left}${result.right}`)
 }
