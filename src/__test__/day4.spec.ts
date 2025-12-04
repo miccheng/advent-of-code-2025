c66d951adea6eb2bfb013d16755f68fd3d3429a8
Michael Cheng <mcheng.work@gmail.com>
2025-12-04 17:17:05 +0800

    Day 4 - Build a grid first.

--- src/__test__/day4.spec.ts
+++ src/__test__/day4.spec.ts
@@ -1,9 +1,15 @@
 import { describe, expect, test } from 'vitest'
-import { solutionPart1, solutionPart2 } from '../day4'
+import { solutionPart1, solutionPart2, gridParser } from '../day4'
 
 describe('Day 4 Tests', () => {
     test('Example Inputs', () => {
         expect(solutionPart1("")).toEqual(0);
         expect(solutionPart2("")).toEqual(0);
+    })
+
+    test('Parser', () => {
+        expect(gridParser('')).toEqual([])
+        expect(gridParser('...')).toEqual([['.', '.', '.']])
+        expect(gridParser("...\n@@@\n...")).toEqual([['.', '.', '.'],['@', '@', '@'],['.', '.', '.']])
     })
 })
--- src/day4.ts
+++ src/day4.ts
@@ -9,3 +9,15 @@ export const solutionPart2 = (input: string) => {
         return acc + 0
     }, 0)
 }
+
+export const gridParser = (input: string): string[][] => {
+    const grid: string[][] = []
+
+    if (input.length === 0) return grid
+
+    for(const row of input.trim().split("\n")) {
+        grid.push(row.split(''))
+    }
+
+    return grid
+}
