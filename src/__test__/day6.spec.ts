0ac9abf829db8af029acee78c246a0822e4594df
Michael Cheng <mcheng.work@gmail.com>
2025-12-06 15:07:34 +0800

    Day 6 part 1 solved!

--- src/__test__/day6.spec.ts
+++ src/__test__/day6.spec.ts
@@ -2,12 +2,24 @@ import { describe, expect, test } from 'vitest'
 import { solutionPart1, solutionPart2, parserInput } from '../day6'
 
 describe('Day 6 Tests', () => {
+    const exampleInput = `123 328  51 64 
+ 45 64  387 23 
+  6 98  215 314
+*   +   *   +  `
+
     test('Example Inputs', () => {
-        expect(solutionPart1("")).toEqual(0);
+        expect(solutionPart1(exampleInput)).toEqual(4277556);
         expect(solutionPart2("")).toEqual(0);
     })
 
     test('Parser', () => {
-        expect(parserInput('')).toEqual([])
+        expect(parserInput(exampleInput)).toEqual({
+            values: [
+                ['123', '328', '51', '64'],
+                ['45', '64', '387', '23'],
+                ['6', '98', '215', '314'],
+            ],
+            operations: ['*', '+', '*', '+']
+        })
     })
 })
--- src/day6.ts
+++ src/day6.ts
@@ -1,15 +1,67 @@
 export const solutionPart1 = (input: string) => {
     const data = parserInput(input)
 
-    return data.length
+    // console.log('Values for 1st row', data.values[0].length)
+    // console.log('Operations', data.operations.length)
+
+    const sheet: number[][] = []
+
+    for(let i=0; i < data.values.length; i++){
+        for(let n=0; n < data.values[i].length; n++ ) {
+            if (!sheet[n]) sheet[n] = []
+            sheet[n].push(parseInt(data.values[i][n]))
+        }
+    }
+
+    let result = 0
+
+    // console.log(sheet)
+    // console.log('Sheet numbers', sheet.length)
+
+    for(let k=0; k < data.operations.length; k++) {
+        let initialValue = data.operations[k] === '+' ? 0 : 1
+        const output = sheet[k].reduce((acc, value) => {
+            if (data.operations[k] === '+') {
+                acc += value
+            } else if (data.operations[k] === '*') {
+                acc = acc * value
+            }
+            return acc
+        }, initialValue)
+
+        console.log('Running Operation', data.operations[k], 'on', sheet[k], 'Output', output)
+
+        result += output
+    }
+
+    return result
 }
 
 export const solutionPart2 = (input: string) => {
     const data = parserInput(input)
 
-    return data.length
+    return 0
 }
 
 export const parserInput = (input:string) => {
-    return []
+    const result: {values: string[][], operations: string[]} = {
+        values: [],
+        operations: []
+    }
+    const rows = input.split("\n")
+    const opsRow = rows.pop()
+
+    rows.map(row => {
+        const matches = row.match(/(\d+)/g)
+        if (matches) {
+            result.values.push(matches)
+        }
+    })
+
+    const operations = opsRow?.match((/[\+\*]/g))
+    if (operations) {
+        result.operations = operations
+    }
+
+    return result
 }
