410fd402d224239206fd48537cd8e0c865b16bc4
Michael Cheng <mcheng.work@gmail.com>
2025-12-06 18:11:54 +0800

    Day 6 Part 2 Solved!

--- src/__test__/day6.spec.ts
+++ src/__test__/day6.spec.ts
@@ -8,18 +8,19 @@ describe('Day 6 Tests', () => {
 *   +   *   +  `
 
     test('Example Inputs', () => {
-        expect(solutionPart1(exampleInput)).toEqual(4277556);
-        expect(solutionPart2("")).toEqual(0);
+        // expect(solutionPart1(exampleInput)).toEqual(4277556);
+        expect(solutionPart2(exampleInput)).toEqual(3263827);
     })
 
     test('Parser', () => {
         expect(parserInput(exampleInput)).toEqual({
             values: [
-                ['123', '328', '51', '64'],
-                ['45', '64', '387', '23'],
-                ['6', '98', '215', '314'],
+                ['123', '328', ' 51', '64 '],
+                [' 45', '64 ', '387', '23 '],
+                ['  6', '98 ', '215', '314'],
             ],
-            operations: ['*', '+', '*', '+']
+            operations: ['*', '+', '*', '+'],
+            colWidth: [3, 3, 3, 3]
         })
     })
 })
--- src/day6.ts
+++ src/day6.ts
@@ -29,7 +29,7 @@ export const solutionPart1 = (input: string) => {
             return acc
         }, initialValue)
 
-        console.log('Running Operation', data.operations[k], 'on', sheet[k], 'Output', output)
+        // console.log('Running Operation', data.operations[k], 'on', sheet[k], 'Output', output)
 
         result += output
     }
@@ -40,28 +40,75 @@ export const solutionPart1 = (input: string) => {
 export const solutionPart2 = (input: string) => {
     const data = parserInput(input)
 
-    return 0
+    // console.log(data)
+
+    let result = 0
+    for(let i=0; i < data.operations.length; i++) {
+        let colResult = data.operations[i] === '+' ? 0 : 1
+        // console.log('Column', i)
+        for(let n=data.colWidth[i]-1; n > -1; n--) {
+            let numStr = ''
+            for(let r=0; r < data.values.length; r++) {
+                numStr += data.values[r][i][n]
+            }
+
+            // console.log('Num', numStr)
+
+            switch(data.operations[i]){
+                case '+':
+                    colResult += parseInt(numStr)
+                    break
+                case '*':
+                    colResult *= parseInt(numStr)
+                    break
+            }
+        }
+        result += colResult
+    }
+
+    return result
 }
 
 export const parserInput = (input:string) => {
-    const result: {values: string[][], operations: string[]} = {
+    const result: {values: string[][], operations: string[], colWidth: number[]} = {
         values: [],
-        operations: []
+        operations: [],
+        colWidth: []
     }
+
     const rows = input.split("\n")
+
     const opsRow = rows.pop()
+    if (opsRow) {
+        const operations = opsRow?.match((/[\+\*]/g))
+        if (operations) {
+            result.operations = operations
+        }
+    
+        for (let i = 0; i < opsRow.length; i++) {
+            const char = opsRow[i];
+            if (['+', '*'].includes(char)) {
+                result.colWidth.push(1)
+                if (i > 0) {
+                    result.colWidth[result.colWidth.length-2] -= 1
+                }
+            }
+            else if (char === ' ') {
+                result.colWidth[result.colWidth.length - 1] += 1
+            }
+        }
+    }
 
     rows.map(row => {
-        const matches = row.match(/(\d+)/g)
-        if (matches) {
-            result.values.push(matches)
+        let pointer = 0
+        const matches: string[] = []
+        for (let k=0; k < result.colWidth.length; k++) {
+            matches.push(row.substring(pointer, pointer + result.colWidth[k]))
+            pointer += result.colWidth[k] + 1
         }
+        
+        result.values.push(matches)
     })
-
-    const operations = opsRow?.match((/[\+\*]/g))
-    if (operations) {
-        result.operations = operations
-    }
 
     return result
 }
