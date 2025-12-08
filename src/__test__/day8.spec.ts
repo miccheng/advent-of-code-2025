f60bcbe612de7d211b35147cc6598392bf89e947
Michael Cheng <mcheng.work@gmail.com>
2025-12-08 21:03:37 +0800

    Day 8, part 1 setup

--- src/__test__/day8.spec.ts
+++ src/__test__/day8.spec.ts
@@ -2,12 +2,33 @@ import { describe, expect, test } from 'vitest'
 import { solutionPart1, solutionPart2, parserInput } from '../day8'
 
 describe('Day 8 Tests', () => {
+    const exampleInput = `162,817,812
+57,618,57
+906,360,560
+592,479,940
+352,342,300
+466,668,158
+542,29,236
+431,825,988
+739,650,466
+52,470,668
+216,146,977
+819,987,18
+117,168,530
+805,96,715
+346,949,466
+970,615,88
+941,993,340
+862,61,35
+984,92,344
+425,690,689`
+
     test('Example Inputs', () => {
-        expect(solutionPart1("")).toEqual(0);
+        expect(solutionPart1(exampleInput)).toEqual(40);
         expect(solutionPart2("")).toEqual(0);
     })
 
     test('Parser', () => {
-        expect(parserInput('')).toEqual([])
+        expect(parserInput(exampleInput)).toEqual([])
     })
 })
