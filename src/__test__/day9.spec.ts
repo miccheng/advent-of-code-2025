5f157501daa42f74376ccb005cd716a5d24f6ce0
Michael Cheng <mcheng.work@gmail.com>
2025-12-20 20:28:53 +0800

    Day 9 Part 1 Solved

--- src/__test__/day9.spec.ts
+++ src/__test__/day9.spec.ts
@@ -1,13 +1,93 @@
 import { describe, expect, test } from 'vitest'
-import { solutionPart1, solutionPart2, parserInput } from '../day9'
+import { solutionPart1, solutionPart2, parserInput, calculateTiles } from '../day9'
 
 describe('Day 9 Tests', () => {
+    const exampleInput = `7,1
+11,1
+11,7
+9,7
+9,5
+2,5
+2,3
+7,3`
+
     test('Example Inputs', () => {
-        expect(solutionPart1("")).toEqual(0);
+        expect(solutionPart1(exampleInput)).toEqual(50);
         expect(solutionPart2("")).toEqual(0);
     })
 
     test('Parser', () => {
-        expect(parserInput('')).toEqual([])
+        expect(parserInput(exampleInput)).toEqual([
+            { col: 7, row: 1 },
+            { col: 11, row: 1 },
+            { col: 11, row: 7 },
+            { col: 9, row: 7 },
+            { col: 9, row: 5 },
+            { col: 2, row: 5 },
+            { col: 2, row: 3 },
+            { col: 7, row: 3 },
+        ])
+    })
+
+    describe('calculateTiles', () => {
+        test('1x1 grid', () => {
+            const p = { col: 0, row: 0 }
+            const q = { col: 0, row: 0 }
+            expect(calculateTiles(p, q)).toEqual(1)
+        })
+
+        test('2x1 grid', () => {
+            const p = { col: 0, row: 0 }
+            const q = { col: 1, row: 0 }
+            expect(calculateTiles(p, q)).toEqual(2)
+        })
+
+        test('1x2 grid', () => {
+            const p = { col: 0, row: 0 }
+            const q = { col: 0, row: 1 }
+            expect(calculateTiles(p, q)).toEqual(2)
+        })
+
+        test('2x2 grid', () => {
+            const p = { col: 0, row: 0 }
+            const q = { col: 1, row: 1 }
+            expect(calculateTiles(p, q)).toEqual(4)
+        })
+
+        test('2x2 grid reversed', () => {
+            const p = { col: 1, row: 0 }
+            const q = { col: 0, row: 1 }
+            expect(calculateTiles(p, q)).toEqual(4)
+        })
+
+        test('3x3 grid', () => {
+            const p = { col: 0, row: 0 }
+            const q = { col: 2, row: 2 }
+            expect(calculateTiles(p, q)).toEqual(9)
+        })
+
+        test('example 1', () => {
+            const p = { col: 2, row: 5 }
+            const q = { col: 9, row: 7 }
+            expect(calculateTiles(p, q)).toEqual(24)
+        })
+
+        test('example 2', () => {
+            const p = { col: 7, row: 1 }
+            const q = { col: 11, row: 7 }
+            expect(calculateTiles(p, q)).toEqual(35)
+        })
+
+        test('example 3', () => {
+            const p = { col: 7, row: 3 }
+            const q = { col: 2, row: 3 }
+            expect(calculateTiles(p, q)).toEqual(6)
+        })
+
+        test('example 4', () => {
+            const p = { col: 2, row: 5 }
+            const q = { col: 11, row: 1 }
+            expect(calculateTiles(p, q)).toEqual(50)
+        })
     })
 })
--- src/day9.ts
+++ src/day9.ts
@@ -1,15 +1,90 @@
+class TileCorners {
+    keys: string[] = []
+    tiles: Tile[] = []
+    totalTiles: number = 0
+
+    constructor(p: Tile, q: Tile) {
+        this.tiles.push(p)
+        this.tiles.push(q)
+        this.keys.push(`${p.col}x${p.row}_${q.col}x${q.row}`)
+        this.keys.push(`${q.col}x${q.row}_${p.col}x${p.row}`)
+    }
+}
+
 export const solutionPart1 = (input: string) => {
     const data = parserInput(input)
 
-    return data.length
+    const uniqueTiles: { [key:string]: TileCorners } = {}
+
+    for (let i = 0; i < data.length; i++) {
+        const p = data[i];
+
+        for (let j = 0; j < data.length; j++) {
+            const q = data[j];
+            
+            if (p.col === q.col && p.row === q.row) {
+                // Same tile
+                continue
+            }
+
+            const cornerCombo = new TileCorners(p, q)
+
+            if (Object.hasOwn(uniqueTiles, cornerCombo.keys[0]) || Object.hasOwn(uniqueTiles, cornerCombo.keys[1])) {
+                continue
+            }
+
+            cornerCombo.totalTiles = calculateTiles(p, q)
+            uniqueTiles[cornerCombo.keys[0]] = cornerCombo
+        }
+    }
+
+    return Object.values(uniqueTiles).sort((a, b) => b.totalTiles - a.totalTiles)[0].totalTiles
 }
 
 export const solutionPart2 = (input: string) => {
     const data = parserInput(input)
 
-    return data.length
+    return 0
 }
 
-export const parserInput = (input:string) => {
-    return []
+export const calculateTiles = (p: Tile, q: Tile): number => {
+    if (p.col === q.col && p.row === q.row) {
+        return 1
+    }
+
+    let width = 0
+    if (p.col === q.col){
+        width = 1
+    } else if (p.col > q.col){
+        width = p.col - q.col + 1
+    } else {
+        width = q.col - p.col + 1
+    }
+
+    let height = 0
+    if (p.row === q.row){
+        height = 1
+    } else if (p.row > q.row){
+        height = p.row - q.row + 1
+    } else {
+        height = q.row - p.row + 1
+    }
+
+    return width * height
+}
+
+export type Tile = {
+    col: number
+    row: number
+}
+
+export const parserInput = (input:string): Tile[] => {
+    const result: Tile[] = []
+
+    input.trim().split("\n").map(rowItem => {
+        const [col, row] = rowItem.split(',')
+        result.push({ col: parseInt(col), row: parseInt(row) })
+    })
+
+    return result
 }
